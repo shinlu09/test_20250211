753164a8baed11ff30f8d3d8559e55ac4ee99ad0
shinlu09~ <adc11.va70@gmail.com~>
2025-02-13 11:32:31 +0800

    手動建立session table 以及建立註冊頁面 php artisan make:session-table php artisan migrate

--- routes/web.php
+++ routes/web.php
@@ -14,18 +14,26 @@ use Illuminate\Support\Facades\Route;
 
 //路由群組設置,可將上方參數合併成一個群組
 
-
 Route::group(['prefix' => 'user'], function () {
-            Route::group(['prefix' => 'auth'], function () {
-                Route::get(
-                'login', 
-                'App\Http\Controllers\UserAuthController@Login'
-            );
-
-            Route::get('signup', 
-            'App\Http\Controllers\UserAuthController@SignUp');   
-            });
+    Route::group(['prefix' => 'auth'], function () {
+        Route::get(
+            'login',
+            'App\Http\Controllers\UserAuthController@Login'
+        );
+        Route::get(
+            'signup',
+            'App\Http\Controllers\UserAuthController@SignUpPage'
+        );
+        Route::post(
+            'signup',
+            'App\Http\Controllers\UserAuthController@SignUpProcess'
+        );
+    });
 });
+    
+    
+
+
 
 
         
